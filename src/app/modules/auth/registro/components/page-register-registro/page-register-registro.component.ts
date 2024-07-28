db13e1921e28715fe1f39033d134ba261e0ef18d
Yeison Monsalve <168552590+YeiMonsalve@users.noreply.github.com>
2024-07-28 13:04:48 -0500

    Integración de la autenticación de Google con Firebase

--- src/app/modules/auth/registro/components/page-register-registro/page-register-registro.component.ts
+++ src/app/modules/auth/registro/components/page-register-registro/page-register-registro.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { ApiGoogleService } from '../../service/api-google/api-google.service';
 
 @Component({
   selector: 'app-page-register-registro',
   standalone: true,
   imports: [],
   templateUrl: './page-register-registro.component.html',
-  styleUrl: './page-register-registro.component.css'
+  styleUrls: ['./page-register-registro.component.css']
 })
 export class PageRegisterRegistroComponent {
+  constructor(private apiGoogleService: ApiGoogleService) {}
 
+  signInWithGoogle() {
+    this.apiGoogleService.signInWithGoogle()
+      .then(result => {
+        console.log('User signed in:', result.user);
+      })
+      .catch(error => {
+        console.error('Error signing in with Google:', error);
+      });
+  }
 }
--- src/app/modules/auth/registro/service/api-google/api-google.service.ts
+++ src/app/modules/auth/registro/service/api-google/api-google.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiGoogleService {
+  private firebaseConfig = {
+    apiKey: "AIzaSyBaYGUmkDa7v-BDY6f_r-JHpPNHwQjEV5s",
+    authDomain: "angular-auth-898ed.firebaseapp.com",
+    projectId: "angular-auth-898ed",
+    storageBucket: "angular-auth-898ed.appspot.com",
+    messagingSenderId: "1079606336153",
+    appId: "1:1079606336153:web:5f34baa521f8999d21b608"
+  };
+
+  private app = initializeApp(this.firebaseConfig);
+  private auth = getAuth(this.app);
 
   constructor() { }
+
+  signInWithGoogle() {
+    const provider = new GoogleAuthProvider();
+    return signInWithPopup(this.auth, provider);
+  }
 }
