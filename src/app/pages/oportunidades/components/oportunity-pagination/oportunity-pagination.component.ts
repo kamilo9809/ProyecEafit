ccdcc5fd33228f3dff521ec994086aa8a921c9e9
kamilo9809 <camilorivera09@outlook.com>
2024-07-29 13:53:01 -0500

    correction leads: adicion de metodo dinamico

--- src/app/pages/oportunidades/components/oportunity-pagination/oportunity-pagination.component.ts
+++ src/app/pages/oportunidades/components/oportunity-pagination/oportunity-pagination.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { componentPagination } from '../../core/pagination';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-oportunity-pagination',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './oportunity-pagination.component.html',
   styleUrl: './oportunity-pagination.component.css'
 })
 export class OportunityPaginationComponent {
+ public oportunitys = componentPagination.slice(0,3)
 
+ moreOportunity(){
+  this.oportunitys.push(this.oportunitys[0],this.oportunitys[1],this.oportunitys[2])
+  }
 }
