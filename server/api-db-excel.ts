7f343699c16fd87cb528323f06fbdca56c27cbbe
Yesenia Luna <yelunape@gmail.com>
2024-07-30 17:27:53 -0500

    feature/feature/creation-database-api-excel/Importo las dependencias y defino la URL de la API SheetDB

---
+++ server/api-db-excel.ts
@@ -0,0 +1,10 @@
+// Importo las dependencias necesarias
+import fetch from 'node-fetch';
+
+// Defino la URL de la API de SheetDB
+const SHEETDB_API_URL = 'https://sheetdb.io/api/v1/oxjtdankgbu46';
+
+
+
+
+
