954849b2eeafd82ecadcdd2c23bef2f7334a32b1
Yeison Monsalve <168552590+YeiMonsalve@users.noreply.github.com>
2024-08-11 19:59:23 -0500

    Creación del list-oportunity-admin usando libreria de bootstrap

--- src/app/modules/users/pages/admin/pages/list-oportunidades/list-oportunidades.component.ts
+++ src/app/modules/users/pages/admin/pages/list-oportunidades/list-oportunidades.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { AsyncPipe, DecimalPipe } from '@angular/common';
+import { Component, QueryList, ViewChildren } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Country } from './core/country';
+import { CountryService } from './services/country.service';
+import { NgbdSortableHeader, SortEvent } from './sortable.directive';
+import { FormsModule } from '@angular/forms';
+import { NgbHighlight, NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-list-oportunidades',
   standalone: true,
-  imports: [],
+  imports: [DecimalPipe, FormsModule, AsyncPipe, NgbHighlight, NgbdSortableHeader, NgbPaginationModule],
   templateUrl: './list-oportunidades.component.html',
-  styleUrl: './list-oportunidades.component.css'
+  styleUrl: './list-oportunidades.component.css',
+  providers: [CountryService, DecimalPipe],
 })
-export class ListOportunidadesComponent {
 
+export class ListOportunidadesComponent {
+	countries$: Observable<Country[]>;
+	total$: Observable<number>;
+
+	@ViewChildren(NgbdSortableHeader)
+  headers!: QueryList<NgbdSortableHeader>;
+
+	constructor(public service: CountryService) {
+		this.countries$ = service.countries$;
+		this.total$ = service.total$;
+	}
+
+	onSort({ column, direction }: SortEvent) {
+		// resetting other headers
+		this.headers.forEach((header) => {
+			if (header.sortable !== column) {
+				header.direction = '';
+			}
+		});
+  }
 }
