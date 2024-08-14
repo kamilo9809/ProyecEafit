ace008a13d669fe933030c76be424937c0dc341f
Yesenia Luna <yelunape@gmail.com>
2024-08-14 15:07:01 -0500

    feature/feature/chat-bot-user/Implementacion de logica para funcionamiento del chat bot

--- src/app/modules/users/pages/home/components/chat-bot/chat-bot.component.ts
+++ src/app/modules/users/pages/home/components/chat-bot/chat-bot.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { chat, chatUser } from './data/chat';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-chat-bot',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './chat-bot.component.html',
   styleUrl: './chat-bot.component.css'
 })
 export class ChatBotComponent {
-
+public chat= chat.map(cnv=>cnv ?? [])
+public chatUser= chatUser.map(cnv=>cnv ?? [])
 }
